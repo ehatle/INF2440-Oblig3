f54cceacabd63666845ffa42e8cfdf5aec01e4dc
Emil <northernmadness@gmail.com>
2018-06-22 22:47:17 -0500

    removed duplicated code

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # INF2440-Oblig3
-Assignment I did back in 2014 for Effective Parallel Programming class
+Assignment I did back in 2014 for Effective Parallel Programming class.
+
+I am currently refactoring the code daily for fun.
 
 ## TODO
-- [ ] remove the swap between arrays in the threads. If I pass the arrays it would remove the problem.
+- [ ] remove the swap between arrays in the threads. If I pass the arrays it should remove the problem.
+- [x] ~~remove duplicated sync method~~
 - [x] ~~eliminate need for digit number in parallel radix~~
 - [x] ~~End parallel sort with a join~~
 - [ ] Remove nested classes:
@@ -10,3 +13,6 @@ Assignment I did back in 2014 for Effective Parallel Programming class
 - [ ] Create methods for altering values
 - [ ] Pass parent object
 - [ ] Translate everything to 1 language
+
+## Notes
+* Test if more efficient to keep nested class structure
--- src/ParaRadix.java
+++ src/ParaRadix.java
@@ -36,7 +36,7 @@ class ParaRadix{
 		threads = new Thread[cores];
 		allCount = new int[cores][];
 		for(int i =0; i<cores-1; i++)
-			threads[i] = new Thread(new RadixThread(i, i*(a.length/cores), (i+1)*(a.length/cores)));
+		threads[i] = new Thread(new RadixThread(i, i*(a.length/cores), (i+1)*(a.length/cores)));
 		threads[cores-1] = new Thread(new RadixThread(cores-1, (cores-1)*(a.length/cores), a.length));
 	}//END constructor
 	/**
@@ -46,9 +46,11 @@ class ParaRadix{
 	**/
 	int[] sort(){
 		for(int i =0; i<cores; i++) threads[i].start();
-		sync();
+		//sync up with main thread
+		sync(cb);
 		sumCount = new int[numDigit];
-		sync();
+		//sync up with main thread
+		sync(cb);
 		sumCount = new int[numDigit];
 
 		for(Thread t : threads){
@@ -68,18 +70,18 @@ class ParaRadix{
 	* @see synchronized
 	**/
 	synchronized void pushMax(int value){
-			if(value > max){
-				max = value;
-			}
-			numBit = 2;
-			while (max >= (1<<numBit))
-				numBit++; //number of bits in max
-			//number of bits in digit1 and digit 2
-			bit[0] = numBit/bit.length;
-			bit[1] = numBit-bit[0];
+		if(value > max){
+			max = value;
+		}
+		numBit = 2;
+		while (max >= (1<<numBit))
+		numBit++; //number of bits in max
+		//number of bits in digit1 and digit 2
+		bit[0] = numBit/bit.length;
+		bit[1] = numBit-bit[0];
 
-			//number of possible digit values
-			updateDigits(0);
+		//number of possible digit values
+		updateDigits(0);
 	}//END pushMax
 	synchronized void updateDigits(int i){
 		numDigit = 1<<bit[i];
@@ -87,131 +89,122 @@ class ParaRadix{
 	/**
 	* A barrier for all threads. Including the main thread.
 	**/
-	void sync() {
+	void sync(CyclicBarrier barrier) {
 		try {// Wait for all the threads to finish
-			cb.await();
+			barrier.await();
 		} catch (BrokenBarrierException e) {// This is triggered when the barrier goes down
 			System.out.println("A thread crossed the streams!");
 		} catch (InterruptedException e) {// This really shouldn't ever happen.
-			System.out.println("Who you gonna call?");
-		}
-	}//END sync
+		System.out.println("Who you gonna call?");
+	}
+}//END sync
+/**
+* Thread class for parallell radix sort
+*
+* @see Thread
+**/
+class RadixThread implements Runnable{
+	int threadNr, start, stop, shift;
 	/**
-	* A barrier for all sorting threads.
-	* NOTE: DOES NOT INCLUDE THE MAIN THREAD.
-	**/
-	void sync1() {
-		try {// Wait for all the threads to finish
-			cb1.await();
-		} catch (BrokenBarrierException e) {// This is triggered when the barrier goes down
-			System.out.println("A thread crossed the streams!");
-		} catch (InterruptedException e) {// This really shouldn't ever happen.
-			System.out.println("Who you gonna call?");
-		}
-	}//END sync1
-	/**
-	* Thread class for parallell radix sort
+	* Constructor
 	*
-	* @see Thread
+	* @param threadNr The current thread's ID number
+	* @param start Inclusive start index in a from which the thread should sort
+	* @param stop Exclusive stop index in a to which the thread should sort
 	**/
-	class RadixThread implements Runnable{
-		int threadNr, start, stop, shift;
-		/**
-		* Constructor
-		*
-		* @param threadNr The current thread's ID number
-		* @param start Inclusive start index in a from which the thread should sort
-		* @param stop Exclusive stop index in a to which the thread should sort
-		**/
-		RadixThread(int threadNr, int start, int stop){
-			this.threadNr = threadNr;
-			this.start = start;
-			this.stop = stop;
-		}//END constructor
-		/**
-		* @see Thread.run()
-		**/
-		public void run(){
-			pushMax(findMax());//TODO: REMOVE PASSING OF CURRENT DIGIT NUMBER
-			radixSort(0);
-			swapAB();//TODO: FIND A WORKAROUND
-			sync1();
-			updateDigits(1);
-			radixSort(bit[0]);
-		}//END run
-		/**
-		* Abstraction barrier
-		*
-		* @param digit The current digit on which to sort
-		* @param shift The the current amount of shift needed to sort on the specified digit
-		**/
-		void radixSort(int shift){
-			this.shift = shift;
+	RadixThread(int threadNr, int start, int stop){
+		this.threadNr = threadNr;
+		this.start = start;
+		this.stop = stop;
+	}//END constructor
+	/**
+	* @see Thread.run()
+	**/
+	public void run(){
+		pushMax(findMax());
+		radixSort(0);
+		swapAB();//TODO: FIND A WORKAROUND
+		//sync up with other threads
+		sync(cb1);
+		updateDigits(1);
+		radixSort(bit[0]);
+	}//END run
+	/**
+	* Abstraction barrier
+	*
+	* @param digit The current digit on which to sort
+	* @param shift The the current amount of shift needed to sort on the specified digit
+	**/
+	void radixSort(int shift){
+		this.shift = shift;
 
-			sync();
-			allCount[threadNr] = countFreq();
-			sync1();
+		//sync up with main thread
+		sync(cb);
+		allCount[threadNr] = countFreq();
+		//sync up with other threads
+		sync(cb1);
 
-			//calculating sumCount
-			int numStop = (threadNr+1)*numDigit/cores;
-			for (int i = threadNr*numDigit/cores; i<numStop; i++){
-				sumCount[i] = 0;
-				for(int j = 0; j<cores; j++) sumCount[i] += allCount[j][i];
-			}
-
-			sync1();
-			sortToB();
-			sync1();
-		}//END radixSort
-		/**
-		* Abstraction barrier.
-		*
-		* @return The highest number in a with an index lower than stop and higher than start
-		**/
-		int findMax(){
-			int myMax = a[start];
-			for (int i = start+1 ; i < stop ; i++)
-				if (a[i] > myMax)
-					myMax = a[i];
-			return myMax;
-		}//END findMax
-		/**
-		* Counts the frequency of all of the current digits in a with an index lower than stop and higher than start
-		*
-		* @return An integer array of the frequencies
-		**/
-		int[] countFreq(){
-			int mask = numDigit -1;
-			int[] count = new int[numDigit];
-			for(int i =start; i<stop; i++)
-				count[(a[i]>> shift) & mask]++;
-			return count;
-		}//END countFreq
-		/**
-		* Sorts all elements in a with an index lower than stop and higher or equal to start into b
-		**/
-		void sortToB(){
-			int mask = numDigit -1;
-			int[] prevCounts = new int[numDigit];
-			for(int i = 0; i<numDigit; i++){
-				for(int j =0; j<i; j++) prevCounts[i] +=sumCount[j];
-				for(int j =0; j<threadNr; j++) prevCounts[i] +=allCount[j][i];
-			}
-			for(int i = start; i<stop; i++){
-				int j = (a[i]>>shift)&mask;
-				b[prevCounts[j]] = a[i];
-				prevCounts[j]++;
-			}
-		}//END sortToB
-		/**
-		* Swaps all elements between a and b
-		**/
-		void swapAB(){
-			for(int i = start; i<stop; i++){
-				int tmp = a[i];
-				a[i] = b[i];
-				b[i] = tmp;
-			}
-		}//END swapAB
-	}//END RadixThread
+		//calculating sumCount
+		int numStop = (threadNr+1)*numDigit/cores;
+		for (int i = threadNr*numDigit/cores; i<numStop; i++){
+			sumCount[i] = 0;
+			for(int j = 0; j<cores; j++) sumCount[i] += allCount[j][i];
+		}
+		//sync up with other threads
+		sync(cb1);
+		sortToB();
+		//sync up with other threads
+		sync(cb1);
+	}//END radixSort
+	/**
+	* Abstraction barrier.
+	*
+	* @return The highest number in a with an index lower than stop and higher than start
+	**/
+	int findMax(){
+		int myMax = a[start];
+		for (int i = start+1 ; i < stop ; i++)
+		if (a[i] > myMax)
+		myMax = a[i];
+		return myMax;
+	}//END findMax
+	/**
+	* Counts the frequency of all of the current digits in a with an index lower than stop and higher than start
+	*
+	* @return An integer array of the frequencies
+	**/
+	int[] countFreq(){
+		int mask = numDigit -1;
+		int[] count = new int[numDigit];
+		for(int i =start; i<stop; i++)
+		count[(a[i]>> shift) & mask]++;
+		return count;
+	}//END countFreq
+	/**
+	* Sorts all elements in a with an index lower than stop and higher or equal to start into b
+	**/
+	void sortToB(){
+		int mask = numDigit -1;
+		int[] prevCounts = new int[numDigit];
+		for(int i = 0; i<numDigit; i++){
+			for(int j =0; j<i; j++) prevCounts[i] +=sumCount[j];
+			for(int j =0; j<threadNr; j++) prevCounts[i] +=allCount[j][i];
+		}
+		for(int i = start; i<stop; i++){
+			int j = (a[i]>>shift)&mask;
+			b[prevCounts[j]] = a[i];
+			prevCounts[j]++;
+		}
+	}//END sortToB
+	/**
+	* Swaps all elements between a and b
+	**/
+	void swapAB(){
+		for(int i = start; i<stop; i++){
+			int tmp = a[i];
+			a[i] = b[i];
+			b[i] = tmp;
+		}
+	}//END swapAB
+}//END RadixThread
 }//END ParaRadix
